29fbe7375d614d12b37c1692bb46734ca3f5f1be
Christopher Beltran <christod.beltran@gmail.com>
2018-05-06 23:57:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,7 @@ function sayHiToGrandma(string) {
     if (string === "I love you, Grandma.") {
       return "I love you, too.";
     }
-    
-var uppercase = shout(string);
-var lowercase = whisper(string);
-
-  if (string.toUpperCase() === uppercase) {
+    if (string.toUpperCase() === uppercase) {
     return "YES INDEED!";
      }
 
